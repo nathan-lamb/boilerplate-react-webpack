64e846ab12ae276626e695bfeb23f4326413040e
Nathan Lamb <nathanlamb022@gmail.com>
2017-08-16 19:54:17 +1200

    Add onChange event

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
 
-const App = () => {
-  return (
-    <h1>React development has begun!</h1>
-  )
+class App extends React.Component {
+  constructor () {
+    super()
+    this.state = {
+      text: ''
+    }
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange (e) {
+    this.setState({
+      text: e.target.value
+    })
+  }
+  render () {
+    return (
+      <div>
+        <input type="text" value={this.state.text} onChange={this.handleChange} />
+        <h4>Tweet Sentiment Analysis</h4>
+        <p>Result:</p>
+      </div>
+    )
+  }
 }
 
 export default App
-
