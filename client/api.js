155d430c1e5dc36b96d1e8c847c043f9295fb681
Nathan Lamb <nathanlamb022@gmail.com>
2017-08-17 07:52:40 +1200

    Link up API

--- client/api.js
+++ client/api.js
@@ -1,12 +1,16 @@
 import request from 'superagent'
-import App from './components/App'
 
 export const getSentiment = (text, cb) => {
   request
-    .get('https://jamiembrown-tweet-sentiment-analysis.p.mashape.com/api')
+    .get('https://jamiembrown-tweet-sentiment-analysis.p.mashape.com/api/')
+    .query({text: text})
     .set('X-Mashape-Key', 'mr7PX3iE3ZmshgkqWCWmdp2cQyb4p1Vj43PjsnoTlpQ4riiYcB')
     .set('Accept', 'application/json')
-    .end(function (result) {
-      console.log(result.status, result.headers, result.body)
+    .end((err, res) => {
+      if (err) {
+        console.error(err.message)
+      } else {
+        cb(res.body.sentiment, res.body.score)
+      }
     })
 }
--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
+import {getSentiment} from '../api'
 
 class App extends React.Component {
   constructor () {
     super()
     this.state = {
-      text: ''
+      text: '',
+      sentiment: '',
+      score: ''
     }
     this.handleChange = this.handleChange.bind(this)
+    this.handleGetAnalysis = this.handleGetAnalysis.bind(this)
   }
 
   handleChange (e) {
@@ -14,12 +18,20 @@ class App extends React.Component {
       text: e.target.value
     })
   }
+
+  handleGetAnalysis () {
+    getSentiment(this.state.text, (sentiment, score) => {
+      this.setState({sentiment: sentiment, score: score})
+    })
+  }
   render () {
     return (
       <div>
         <input type="text" value={this.state.text} onChange={this.handleChange} />
+        <button onClick={this.handleGetAnalysis}>Submit</button>
         <h4>Tweet Sentiment Analysis</h4>
-        <p>Result:</p>
+        <p>Sentiment: {this.state.sentiment} </p>
+        <p>Score: {this.state.score}</p>
       </div>
     )
   }
